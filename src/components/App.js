f4acdce40ad9deb4951a377d1ac348196d6226cf
JaGretz <jason.gretz@dominos.com>
2017-07-12 16:55:44 -0400

    Setup of the initial application structure - Added styles and imported bootstrap - Added Routing for index, about, and course routes

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from "react";
+import Header from "../components/common/Header";
+
+/**
+ * Handles the application template used on every page.
+ */
+export default class App extends Component {
+  render() {
+    return(
+      <div className="container-fluid">
+        <Header />
+        {/* Passing down, recieved as props, from the router */}
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import { Link } from "react-router";
+
+export default class AboutPage extends Component {
+  render () {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application does stuff using react, redux, react router and some helpful libs built, of
+          course, with webpack.</p>
+      </div>
+    );
+  }
+}
