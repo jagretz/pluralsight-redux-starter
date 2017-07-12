7afd3f2875c0bf32e951be6697cd187ba9f42d91
JaGretz <jason.gretz@dominos.com>
2017-07-11 20:21:51 -0400

    Update configuration files - Everything can be run from the npm scripts, including hmr, lint, and tests.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,7 +4,7 @@ import path from 'path';
 export default {
   debug: true,
   devtool: 'inline-source-map',
-  noInfo: false,
+  noInfo: true,
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
@@ -25,8 +25,15 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loaders: ['babel']
+      },
+      {
+        test: /(\.css)$/,
+        loaders: ['style', 'css']
+      },
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
